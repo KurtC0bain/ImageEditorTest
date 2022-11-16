d29c7fcebb3e4091f08bd626b43eb1edac3a0048
Maksym Pishchuk <maksym.pishchuk@unity-bars.com>
2022-11-17 01:17:23 +0200

    Revert "select and cut fix"
    This reverts commit b4ac7eb69a342d026d59a68f3b2eab316e7030b4.

--- ImageEditorTest/ImageEditForm.cs
+++ ImageEditorTest/ImageEditForm.cs
@@ -1,4 +1,8 @@
-﻿namespace ImageEditorTest
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace ImageEditorTest
 {
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
     public partial class ImageEditForm : Form, IDisposable
@@ -15,17 +19,21 @@
             pictureBox1.Parent = this;
             panelCrop.Parent = pictureBox1;
             panelCrop.MovableAndDraggable();
+
+            panelCrop.Visible = false;
         }
 
-        private void buttonRotateMinus90_Click(object sender, EventArgs e) => this.RotateImage(RotateFlipType.Rotate270FlipNone);
-        private void buttonRotatePlus90_Click(object sender, EventArgs e) => this.RotateImage(RotateFlipType.Rotate90FlipNone);
-        private void buttonReset_Click(object sender, EventArgs e) => this.SetImage(_imagePath);
+        /// <summary>
+        /// Event handlers
+        /// </summary>
+        /// <param name="sender">obejct which invokes action</param>
+        /// <param name="e">parameters</param>
+        private void buttonRotateMinus90_Click(object sender, EventArgs e) => this.RotateImage(270);
+        private void buttonRotatePlus90_Click(object sender, EventArgs e) => this.RotateImage(90);
 
-        private void buttonSave_Click(object sender, EventArgs e)
-        {
-            var img = pictureBox1.Image;
-            img.Save(_imagePath);
-        }
+        private void buttonReset_Click(object sender, EventArgs e) => this.ResetImage();
+
+        private void buttonSave_Click(object sender, EventArgs e) => this.SaveImage();
 
         private void checkBoxCrop_CheckedChanged(object sender, EventArgs e)
         {
@@ -33,38 +41,112 @@
 
             pictureBox1.Cursor = checkbox.Checked ? Cursors.Cross : Cursors.Default;
 
-            if (checkbox.Checked)
+            if(checkbox.Checked)
             {
-                //CropImage(new Rectangle(panelCrop.Location, panelCrop.Size));
+                panelCrop.Visible = true;
+            }
+            else if(panelCrop.Visible)
+            {
                 CropImage(new Rectangle(panelCrop.Location.X, panelCrop.Location.Y, panelCrop.Size.Width, panelCrop.Size.Height));
+
+                panelCrop.Size = new Size(pictureBox1.Width, pictureBox1.Height);
+                panelCrop.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y);
+
+                checkbox.Checked = !checkbox.Checked;
             }
         }
 
 
-
+        /// <summary>
+        /// Editor methods
+        /// </summary>
         private void SetImage(string imagePath)
         {
             pictureBox1.Image?.Dispose();
             pictureBox1.Image = Image.FromFile(imagePath);
             this.UpdateInfo();
         }
-
-        private void RotateImage(RotateFlipType rotateFlipType)
+        private void SaveImage()
         {
             var currentImg = pictureBox1.Image;
-            currentImg.RotateFlip(rotateFlipType);
+
+            SaveFileDialog sf = new();
+            sf.FileName = "croped_image";
+            sf.Filter = "JPG(*.JPG)|*.jpg";
+
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                currentImg.Save(sf.FileName);
+            }
+
+        }
+        private void ResetImage()
+        {
+            this.SetImage(_imagePath);
+
+            panelCrop.Visible = false;
+            checkBoxCrop.Checked = false;
+
+            panelCrop.Location = new Point(297, 178);
+            panelCrop.Size = new Size(200, 100);
+        }
+
+        private void RotateImage(int angle)
+        {
+/*                       var currentImg = pictureBox1.Image;
+                pictureBox1.Image = currentImg.RotateFlip(rotateFlipType);
+*/
+
+   
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+            int w = bmp.Width;
+
+            int h = bmp.Height;
+
+            Bitmap tempImg = new Bitmap(w, h);
+
+            Graphics g = Graphics.FromImage(tempImg);
+
+            g.DrawImageUnscaled(bmp, 1, 1);
+
+            g.Dispose();
+
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddRectangle(new RectangleF(0f, 0f, w, h));
+
+            Matrix mtrx = new Matrix();
+
+            mtrx.Rotate(angle);
+
+            RectangleF rct = path.GetBounds(mtrx);
+
+            Bitmap newImg = new Bitmap(Convert.ToInt32(rct.Width), Convert.ToInt32(rct.Height));
+
+            g = Graphics.FromImage(newImg);
+
+            g.TranslateTransform(-rct.X, -rct.Y);
+
+            g.RotateTransform(angle);
+
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+
+            g.DrawImageUnscaled(tempImg, 0, 0);
+
+            g.Dispose();
+
+            tempImg.Dispose();
+
+            pictureBox1.Image = newImg;
             pictureBox1.Invalidate();
 
             this.UpdateInfo();
         }
 
-
         private void CropImage(Rectangle rect)
         {
-            //var currentImg = pictureBox1.Image as Bitmap;
-            Bitmap currentImg = new(pictureBox1.Image,
-                                             pictureBox1.Width,
-                                             pictureBox1.Height);
+            Bitmap currentImg = new(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
             if (currentImg is { })
             {
                 try
@@ -73,7 +155,9 @@
                     pictureBox1.Image = newImg;
                     currentImg.Dispose();
 
-                    this.UpdateInfo();
+                    panelCrop.Size = newImg.Size;
+
+                    UpdateInfo();
                 }
                 catch (Exception)
                 {
@@ -89,7 +173,6 @@
             //update info
             labelImageSize.Text = $"{img.Width}x{img.Height}";
         }
-
 
         public new void Dispose()
         {
